b9b107562dff809a91f955d744eaafc39576029e
EmmoKitsune <123168788+EmmoKitsune@users.noreply.github.com>
2024-09-21 19:11:52 -0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,74 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+#include <QMessageBox>
+#include <QProcess>
+#include <QStringList>
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+QString URL = "";
+QString downloadDir = "";
+
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_urlLineEdit_textChanged(const QString &arg1)
+{
+    URL = arg1;
+}
+
+
+void MainWindow::on_downloadButton_clicked()
+{
+    if(!URL.isEmpty())
+    {
+        QProcess *process = new QProcess();
+        QStringList arguments;
+
+        downloadDir = QFileDialog::getExistingDirectory(this,"Chose download directory","/home",QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+        if(!downloadDir.isEmpty()){
+
+            arguments << "-f" << "mp4";
+            arguments << "--output" << downloadDir + "/%(title)s.%(ext)s" << URL;
+
+            process->start("yt-dlp",arguments);
+
+            if(!process->waitForStarted())
+            {
+                QMessageBox::information(this,"Error","Error downloading media");
+                delete process;
+                return;
+            }
+            if(process->waitForFinished())
+            {
+                QMessageBox::information(this,"Info","Media downloaded!");
+            }
+
+            process->deleteLater();
+        }
+        else
+        {
+            QMessageBox::information(this,"Error","Media path is empty");
+            //Potencial leak of memory pointed to by 'process' [clang-analizer-cplusplus.NewDeleteLeaks]
+            //fuck.........
+            return;
+        }
+    }
+    else
+    {
+        QMessageBox::information(this,"Error","URL is empty");
+        return;
+    }
+}
+
